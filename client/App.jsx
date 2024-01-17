d9a0957d8ce429da6d9e5659e2fc2dc4cbaaa23e
binzheng622 <binzheng622@gmail.com>
2024-01-17 11:30:44 -0500

    pokemon guess added

--- client/App.jsx
+++ client/App.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import MainContainer from './containers/MainContainer.jsx';
 import Header from './pages/Header.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import '../client/styles.scss'
+import '../client/styles.scss';
 
 const App = () => {
   return (
-      <div className="base">
-        <Login />
-        <MainContainer />
-      </div>
+    <div className='base'>
+      <MainContainer />
+    </div>
   );
 };
 
