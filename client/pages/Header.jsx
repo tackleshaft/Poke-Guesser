a58727ac8de6562b61f5b5cbdab65853afba582a
Laura <laforden721@gmail.com>
2024-01-17 12:37:05 -0800

    adjustments

--- client/pages/Header.jsx
+++ client/pages/Header.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import Logo from '../assets/pokemonLogo.png';
 import Who from '../assets/whoTitle.png';
-import Login from './Login.jsx';
 
 const Header = (props) => {
-  const { updateUserInfo } = props
 
   return (
     <div className='header'>
